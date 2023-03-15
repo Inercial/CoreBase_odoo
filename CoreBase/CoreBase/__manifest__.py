e021948b6ed1ca144426f256dc0ab2ed42124883
julian <julianmanuel42@gmail.com>
2023-03-15 12:54:23 -0600

    modificados:     CoreBase/CoreBase/__manifest__.py

--- CoreBase/CoreBase/__manifest__.py
+++ CoreBase/CoreBase/__manifest__.py
@@ -15,5 +15,6 @@
     ],
     'demo': [
     ],
-    'application': False,#change the value of this field if you need to see the you module like another module
+    'application': True,#Change the value of this field if you need to see the you module like another module
+                        #True for see you module as a aplication, False for ose this module to change the funtions of a other module
 }
